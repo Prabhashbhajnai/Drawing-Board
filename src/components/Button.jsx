2d6dd0e46acb618ffb2e34f16e5b0609533e532e
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-02-27 13:54:42 +0530

    Active states added for buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import { useCanvas } from "../context/CanvasContext"
 
 const Button = ({ type }) => {
-    const { canvasRef, setIsDrawing, isDrawing, isPencil, setIsPencil } = useCanvas()
+    const { canvasRef, isPencil, setIsPencil, isEraser, setIsEraser } = useCanvas()
 
     const handleClear = () => {
         const canvas = canvasRef.current
@@ -12,23 +12,24 @@ const Button = ({ type }) => {
 
     const handleDraw = () => {
         setIsPencil(!isPencil)
+        setIsEraser(false)
     }
 
     const handleErase = () => {
-
+        setIsPencil(false)
+        setIsEraser(!isEraser)
     }
 
     const handleClick = () => {
         if (type.name === 'pencil') handleDraw()
-        else if (type.name === 'eraser') console.log('Eraser')
+        else if (type.name === 'eraser') handleErase()
         else if (type.name === 'clear') handleClear()
     }
-
 
     return (
         <div
             onClick={handleClick}
-            className="flex justify-center items-center bg-slate-500 w-16 h-16 cursor-pointer rounded-b-lg"
+            className={`flex justify-center items-center bg-slate-500 w-16 h-16 cursor-pointer rounded-b-lg hover:bg-slate-700 ${isPencil && type.name === 'pencil' ? 'bg-slate-600' : ''} ${isEraser && type.name === 'eraser' ? 'bg-slate-600' : ''}`}
         >
             {type.icon}
         </div>
--- src/context/CanvasContext.jsx
+++ src/context/CanvasContext.jsx
@@ -5,8 +5,10 @@ const initialState = {
     canvasRef: null,
     isDrawing: false,
     isPencil: false,
+    isEraser: false,
     setIsDrawing: () => { },
-    setIsPencil: () => { }
+    setIsPencil: () => { },
+    setIsEraser: () => { }
 }
 
 // Create a context for the canvas
@@ -20,13 +22,16 @@ const CanvasProvider = ({ children }) => {
     const canvasRef = useRef()
     const [isDrawing, setIsDrawing] = useState(false);
     const [isPencil, setIsPencil] = useState(true);
+    const [isEraser, setIsEraser] = useState(false);
 
     const value = {
         canvasRef,
         isDrawing,
         isPencil,
+        isEraser,
         setIsDrawing,
-        setIsPencil
+        setIsPencil,
+        setIsEraser
     }
 
     return (
