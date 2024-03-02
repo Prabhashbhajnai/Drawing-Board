4e7ce0a125b2fea0f2aa6838e3d8f3474d62ebac
Prabhashbhajani <prabhashbhajani@gmail.com>
2024-03-02 13:02:52 +0530

    Color Palette added

--- src/components/ColorPalette.jsx
+++ src/components/ColorPalette.jsx
@@ -1,30 +1,50 @@
 const ColorPalette = () => {
+    const colors = [
+        { name: 'black', hex: '000000' },
+        { name: 'white', hex: 'ffffff' },
+        { name: 'red', hex: 'ff0000' },
+        { name: 'yellow', hex: 'ffff00' },
+        { name: 'green', hex: '008000' },
+        { name: 'orange', hex: 'ffa500' },
+        { name: 'pink', hex: 'ffc0cb' },
+        { name: 'slate', hex: '708090' },
+        { name: 'sky blur', hex: '00ccff' },
+        { name: 'dark blue', hex: '0000cc' },
+        { name: 'lime', hex: '00ff00' },
+        { name: 'purple', hex: '800080' },
+        { name: 'violet', hex: '8a2be2' },
+        { name: 'indigo', hex: '4b0082' },
+        { name: 'blue', hex: '0000ff' },
+        { name: 'cyan', hex: '00ffff' },
+        { name: 'magenta', hex: 'ff00ff' },
+        { name: 'gold', hex: 'ffd700' },
+        { name: 'silver', hex: 'c0c0c0' },
+        { name: 'grey', hex: '808080' },
+        { name: 'maroon', hex: '800000' },
+        { name: 'olive', hex: '808000' },
+        { name: 'navy', hex: '000080' },
+        { name: 'teal', hex: '008080' },
+        { name: 'aqua', hex: '00ffff' },
+        { name: 'fuchsia', hex: 'ff00ff' },
+        { name: 'lime', hex: '00ff00' },
+        { name: 'dark green', hex: '006400' },
+        { name: 'dark red', hex: '8b0000' },
+        { name: 'dark yellow', hex: '808000' },
+    ];
+
     return (
         <>
             <div className="flex flex-wrap w-full">
-                <div className="w-5 h-5 bg-black" />
-                <div className="w-5 h-5 bg-white" />
-                <div className="w-5 h-5 bg-red-400" />
-                <div className="w-5 h-5 bg-yellow-500" />
-                <div className="w-5 h-5 bg-green-500" />
-                <div className="w-5 h-5 bg-orange-500" />
-                <div className="w-5 h-5 bg-pink-400" />
-                <div className="w-5 h-5 bg-slate-400" />
-                <div className="w-5 h-5 bg-black" />
-                <div className="w-5 h-5 bg-black" />
-                <div className="w-5 h-5 bg-black" />
-                <div className="w-5 h-5 bg-white" />
-                <div className="w-5 h-5 bg-red-400" />
-                <div className="w-5 h-5 bg-yellow-500" />
-                <div className="w-5 h-5 bg-green-500" />
-                <div className="w-5 h-5 bg-orange-500" />
-                <div className="w-5 h-5 bg-pink-400" />
-                <div className="w-5 h-5 bg-slate-400" />
-                <div className="w-5 h-5 bg-black" />
-                <div className="w-5 h-5 bg-black" />
+                {colors.map((color, index) => (
+                    <div
+                        style={{ backgroundColor: `#${color.hex}` }}
+                        className="w-5 h-5"
+                        key={index}
+                    />
+                ))}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default ColorPalette
+export default ColorPalette;
